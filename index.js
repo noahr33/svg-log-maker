b0febb353fd89c9b5b244c2fc1769400a5b7709b
Noah <noahkoleske@icloud.com>
2023-08-14 22:00:54 -0500

    fixed generateLogo

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import inquirer from "inquirer"
-import fs from "fs"
-// import { Circle } from "./lib/shapes.js"
-// import { Triangle } from "./lib/shapes.js"
-// import { Square } from "./lib/shapes.js"
-// import { generateLogo } from "./lib/generateLogo.js"
+import { generateLogo } from "./lib/generateLogo.js"
 
 
 inquirer.prompt([
@@ -21,7 +17,7 @@ inquirer.prompt([
         type: 'list',
         name: 'shape',
         message: 'Which shape would you like your logo to be?',
-        choices: ['circle', 'triangle', 'sqaure']
+        choices: ['circle', 'triangle', 'square']
     },
     {
         type: 'input',
@@ -30,103 +26,8 @@ inquirer.prompt([
     }
 ])
     .then(data => {
-        
-        const logo = generateLogo(data)
-        fs.writeFile(`./examples/${data.title}.svg`,
-        logo, err => {
-            if (err) {
-                throw err
-            }
-            console.log(`${data.text}.svg saved!`)
-        })
+        generateLogo(`logo.svg`, data)
     })
 
 
 
-
-
-
-//     {/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-//   <circle cx="150" cy="100" r="80" fill="green" />
-//   <text x="150" y="125" font-size="60" text-anchor="middle" fill="blue">TAJ</text>
-// </svg> */}
-
-class Component {
-    constructor(children) {
-      this.children = children ? children : []
-    }
-  
-    render() {
-      throw new Error('Child class must implement a render method')
-    }
-  
-    renderInnerXML() {
-      let innerHTML = ''
-      for (const child of this.children) {
-        if (typeof child === 'string') {
-          innerHTML += child
-        } else {
-          innerHTML += child.render()
-        }
-      }
-      return innerHTML
-    }
-  }
-  
-  class SVG extends Component {
-    constructor(children) {
-      super(children)
-    }
-  
-    render() {
-      return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        ${this.renderInnerXML()}
-      </svg>`
-    }
-    shapeText(text, color) {
-        return  `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-            }
-  }
-  
-  class Circle extends Component {
-    render() {
-      return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
-    }
-  }
-
-  class Triangle extends Component {
-    render() {
-        return `<rect x="50" width="200" height="200" fill=${this.shapeColor}/>`
-    }
-  }
-
-class Square extends Component {
-    render() {
-        return `<polygon points="150, 18 244, 182 56, 182" fill=${this.shapeColor}/>`
-    }
-}
-  
-  const circle = new Circle()
-  const triangle = new Triangle()
-  const sqaure = new Square()
-
-  const svg = new SVG([`${this.shape}`])
-  const result = svg.render()
-  console.log(result)
-  
-  function generateLogo(data) {
-    const answers = inquirer.prompt(data)
-    let shape = answers.shape
-    let text = answers.text
-    let textColor = answers.textColor
-    let shapeColor = answers.shapeColor
-
-    
-    if (shape === "circle" || shape === "Circle") {
-        shape = circle.render()
-    } else if (shape === "triangle" || shape === "Triangle") {
-        shape = triangle
-    } else {shape === "square" || shape === "Sqaure"} {
-        shape = sqaure
-    }
-}
